f41dcdcd4c3bfd4445d0ce8008b881ef3936f8d1
Anand Singh <anand.singh@indixpert.com>
2026-05-11 17:30:36 +0530

    Updating Javascript Number and String Class

---
+++ Javascript/Day-1-Introduction/main.js
@@ -0,0 +1,11 @@
+//console.log("From main.js")
+
+// jksahfdkhf
+// kjashdfkj
+// kjhaskjdhf
+// jahkdhf 
+
+console.log("after comment")
+console.log(9999999999999999n + 1n)
+let arr = [1, 2, 3, 4, 5]
+console.log(typeof arr) // output?
